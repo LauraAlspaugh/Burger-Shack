a71e01a0b6c7c0b98e3398edb940594f984c0011
Laura <lauraalspaugh2002@icloud.com>
2023-10-16 15:45:15 -0600

    change

--- src/controllers/BurgersController.js
+++ src/controllers/BurgersController.js
@@ -7,14 +7,25 @@ import { BadRequest } from "../utils/Errors.js";
 
 export class BurgersController extends BaseController {
     constructor() {
-        super('api/burgers');
+        super('api/burgers')
         this.router
             .get('', this.getBurgers)
+            .get('/:burgerId', this.getBurgerById)
     }
     async getBurgers(request, response, next) {
         try {
             const burgers = await burgersService.getBurgers()
+            // throw new BadRequest('testing error')
             return response.send(burgers)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getBurgerById(request, response, next) {
+        try {
+            const burgerId = request.params.burgerId
+            const burger = await burgersService.getBurgerById(burgerId)
+            return response.send(burger)
         } catch (error) {
             next(error)
         }
--- src/services/BurgersService.js
+++ src/services/BurgersService.js
@@ -3,8 +3,16 @@ import { Burger } from "../models/Burger.js";
 import { BadRequest } from "../utils/Errors.js";
 
 class BurgersService {
-    getBurgers() {
-
+    async getBurgerById(burgerId) {
+        const foundBurger = await FakeBurgerDb.burgers.find(burger => burger.id == burgerId)
+        if (!foundBurger) {
+            throw new BadRequest('this is not a valid id')
+        }
+        return foundBurger
+    }
+    async getBurgers() {
+        const burgers = await FakeBurgerDb.burgers
+        return burgers
     }
 }
 export const burgersService = new BurgersService()
